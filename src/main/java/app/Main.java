afb90591cfc0e7be4e79da3364ca41f8ca9224f3
= <=>
2025-12-29 10:51:23 -0500

    Initial commit of Java project

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,6 +4,7 @@ import main.java.app.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,13 +13,64 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        ArrayList<String> femaleNames = new ArrayList<>(List.of(
+                "Emma", "Olivia", "Sophia", "Amelia", "Isabella",
+                "Mia", "Ava", "Charlotte", "Luna", "Harper"
+        ));
+
+
+
+        ArrayList<String> maleNames = new ArrayList<>(List.of(
+                "Liam", "Noah", "Oliver", "James", "Elijah",
+                "William", "Henry", "Lucas", "Mason", "Logan"
+        ));
+
+
+        ArrayList<String> lastNames = new ArrayList<>(List.of(
+                "Smith", "Johnson", "Williams", "Brown", "Jones",
+                "Garcia", "Miller", "Davis", "Rodriguez", "Martinez"
+        ));
+
+
+
+        boolean isFemale, hasMiddleName;
+        Random randomGenerator = new Random();
+
+
+
         List<User> userList = new ArrayList<User>();
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 15; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>
-            User user = new User(i);
-            userList.add(user);
+            isFemale = randomGenerator.nextBoolean();
+            hasMiddleName = randomGenerator.nextBoolean();
+            int firstNameIdx = randomGenerator.nextInt(10);
+            int middleNameIdx = randomGenerator.nextInt(10);
+            int lastNameIdx = randomGenerator.nextInt(10);
+
+            User newUser;
+            if(isFemale) {
+
+                if( hasMiddleName ) {
+                    newUser = new User(i, femaleNames.get(firstNameIdx), femaleNames.get(middleNameIdx), lastNames.get(lastNameIdx), randomGenerator.nextInt(120));
+                } else {
+                    newUser = new User(i, femaleNames.get(firstNameIdx), lastNames.get(lastNameIdx), randomGenerator.nextInt(120));
+                }
+            } else {
+                if( hasMiddleName ) {
+                    newUser = new User(i, maleNames.get(firstNameIdx), maleNames.get(middleNameIdx), lastNames.get(lastNameIdx), randomGenerator.nextInt(120));
+                } else {
+                    newUser = new User(i, maleNames.get(firstNameIdx), lastNames.get(lastNameIdx), randomGenerator.nextInt(120));
+                }
+
+            }
+
+            userList.add(newUser);
+        }
+
+        for (User user : userList) {
+            System.out.println(user.toString());
         }
     }
 
--- src/main/java/app/model/User.java
+++ src/main/java/app/model/User.java
@@ -1,26 +1,39 @@
 package main.java.app.model;
 
+import java.util.Optional;
+
 public class User {
     private final int id;
     private final String firstName;
-//    private final Optional<String> middleName;
+    private Optional<String> middleName;
     private final String lastName;
     private final int age;
 
-    User(int id, String firstName, String lastName, int age) {
+    public User(int id, String firstName, String lastName, int age) {
         this.id = id;
         this.firstName = firstName;
-//        this.middleName = middleName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public User(int id, String firstName, String middleName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = Optional.of(middleName);
         this.lastName = lastName;
         this.age = age;
     }
 
     public User(int id) {
-        this.id = id;
-        this.firstName = "";
-        this.lastName = "";
-        this.age = 0;
+        this(id, "", "", 0);
     }
+
+    @Override
+    public String toString() {
+        return "User{name='" + firstName + " " +  (middleName != null ? middleName.get() + " " : "")  + lastName + ", age: " + age + "'}";
+    }
+
+
 
 
 
